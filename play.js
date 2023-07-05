fcb9556208f496226b43ce31b02409c3930c1451
AlexWBroughton <ab.alex.broughton@gmail.com>
2023-07-05 23:42:31 +0000

    play is DRY

--- play.js
+++ play.js
@@ -1,28 +1,14 @@
 const net = require("net");
-const connect = require("./client");
-
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  process.stdin.on("data", handleUserInput);
-  return stdin;
-};
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 
 
 
-setupInput();
+
+
+
 
 console.log("Connecting ...");
 connect();
+setupInput();
